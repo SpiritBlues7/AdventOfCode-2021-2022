8d586d3b3fc2b61a5ac5927761ae50e857fecbd0
Brian Stewart <brianstewarteditor@gmail.com>
2021-12-07 12:44:06 +1000

    added default template

--- AdventOfCode/Solutions/Year2021/Day07/Day07.cs
+++ AdventOfCode/Solutions/Year2021/Day07/Day07.cs
@@ -1,24 +1,28 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace AdventOfCode.Solutions.Year2021
 {
 
-    class Day07 : ASolution
+    class Day00 : ASolution
     {
+
 
         public Day07() : base(07, 2021, "")
         {
 
         }
 
-        protected override string SolvePartOne()
+        protected override string SolvePartOne(string input)
         {
-            return null;
+            string[] lines = input.SplitByNewline();
+            return "";
         }
 
-        protected override string SolvePartTwo()
+        protected override string SolvePartTwo(string input)
         {
             return null;
         }
--- AdventOfCode/Solutions/Year2021/Day25/Day25.cs
+++ AdventOfCode/Solutions/Year2021/Day25/Day25.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace AdventOfCode.Solutions.Year2021
 {
@@ -8,17 +10,19 @@ namespace AdventOfCode.Solutions.Year2021
     class Day25 : ASolution
     {
 
+
         public Day25() : base(25, 2021, "")
         {
 
         }
 
-        protected override string SolvePartOne()
+        protected override string SolvePartOne(string input)
         {
-            return null;
+            string[] lines = input.SplitByNewline();
+            return "";
         }
 
-        protected override string SolvePartTwo()
+        protected override string SolvePartTwo(string input)
         {
             return null;
         }
