3eeefcbf048bca617f3a5b0581511cf3946e407d
Brian Stewart <brianstewarteditor@gmail.com>
2022-12-05 14:47:33 +1000

    2022 day 3 and 4

--- AdventOfCode/Solutions/Year2022/Day03/Day03.cs
+++ AdventOfCode/Solutions/Year2022/Day03/Day03.cs
@@ -20,12 +20,67 @@ namespace AdventOfCode.Solutions.Year2022
             input = input.Replace("\r\n", "\n");
             string[] lines = input.SplitByNewline();
 
-            return null;
+            int total = 0;
+            foreach (string line in lines)
+            {
+                int count = line.Count();
+                string first = line.Substring(0, count/2);
+                string second = line.Substring(count/2);
+
+                foreach (char x in first)
+                {
+                    if (second.Contains(x))
+                    {
+                        int temp = x - 'a' + 1;
+                        if (x - 'a' < 0)
+                        {
+                            temp = x - 'A' + 1;
+                            temp += 26;
+                        }
+
+                        total += temp;
+                        break;
+                    }
+                }
+
+            }
+
+            return total.ToString();
         }
 
         protected override string SolvePartTwo(string input)
         {
-            return null;
+            input = input.Replace("\r\n", "\n");
+            string[] lines = input.SplitByNewline();
+
+            int total = 0;
+
+            for (int i = 0; i < lines.Length; i+=3)
+            {
+                string first = lines[i];
+                string second = lines[i + 1];
+                string third = lines[i + 2];
+
+                foreach (char x in first)
+                {
+                    if (second.Contains(x))
+                    {
+                        if (third.Contains(x))
+                        {
+                            int temp = x - 'a' + 1;
+                            if (x - 'a' < 0)
+                            {
+                                temp = x - 'A' + 1;
+                                temp += 26;
+                            }
+                            total += temp;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return total.ToString();
         }
     }
 }
--- AdventOfCode/Solutions/Year2022/Day04/Day04.cs
+++ AdventOfCode/Solutions/Year2022/Day04/Day04.cs
@@ -20,13 +20,72 @@ namespace AdventOfCode.Solutions.Year2022
         {
             input = input.Replace("\r\n", "\n");
             string[] lines = input.SplitByNewline();
+            long total = 0;
+            foreach (string line in lines)
+            {
+                string[] components = line.Split(",");
+                string first = components[0];
+                string second = components[1];
 
-            return null;
+                string[] firstComp = first.Split("-");
+                string[] secondComp = second.Split("-");
+
+                int first_first = int.Parse(firstComp[0]);
+                int first_second = int.Parse(firstComp[1]);
+
+                int second_first = int.Parse(secondComp[0]);
+                int second_second = int.Parse(secondComp[1]);
+
+                if (first_first <= second_first && first_second >= second_second)
+                {
+                    total++;
+                    continue;
+                }
+
+                if (first_first >= second_first && first_second <= second_second)
+                {
+                    total++;
+                    continue;
+                }
+            }
+            
+            return total.ToString();
         }
 
         protected override string SolvePartTwo(string input)
         {
-            return null;
+            input = input.Replace("\r\n", "\n");
+            string[] lines = input.SplitByNewline();
+            long total = 0;
+            foreach (string line in lines)
+            {
+                string[] components = line.Split(",");
+                string first = components[0];
+                string second = components[1];
+
+                string[] firstComp = first.Split("-");
+                string[] secondComp = second.Split("-");
+
+                int first_first = int.Parse(firstComp[0]);
+                int first_second = int.Parse(firstComp[1]);
+
+                int second_first = int.Parse(secondComp[0]);
+                int second_second = int.Parse(secondComp[1]);
+
+                if (first_first <= second_first && first_second >= second_first)
+                {
+                    total++;
+                    continue;
+                }
+
+                if (second_first <= first_first && second_second >= first_first)
+                {
+                    total++;
+                    continue;
+                }
+            }
+
+            return total.ToString();
         }
     }
 }
