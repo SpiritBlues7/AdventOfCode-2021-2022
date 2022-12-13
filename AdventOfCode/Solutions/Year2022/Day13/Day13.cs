9914596946224e98603dc036c16376d9182f8710
Brian Stewart <brianstewarteditor@gmail.com>
2022-12-13 23:11:08 +1000

    tidy up day 13

--- AdventOfCode/Solutions/Year2022/Day13/Day13.cs
+++ AdventOfCode/Solutions/Year2022/Day13/Day13.cs
@@ -23,28 +23,15 @@ namespace AdventOfCode.Solutions.Year2022
             string[] pairs = input.Split("\n\n");
             long sum = 0;
 
-            // Fix for strange bug with input
-            string line0a = "[[[],2,9,4],[],[[[],[0]],[[7,0,10,8],[10,5,5,4,1],0,6]],[]]";
-            string line0b = "[[[8],[3,6,9,[10,4,2],[4,5]],[10,[3],[7,10,7,2],[0,5]],2]]";
-            List<dynamic> line0aList = ConvertLineToList(line0a);
-            List<dynamic> line0bList = ConvertLineToList(line0b);
-            int inOrderFix = CompareTwoLists(line0aList, line0bList);
-            if (inOrderFix == -1)
-            {
-                sum += 1;
-            }
-
             for (int i = 0; i < pairs.Count(); i++)
             {
                 string[] lines = pairs[i].SplitByNewline();
                 List<dynamic> line1 = ConvertLineToList(lines[0]);
                 List<dynamic> line2 = ConvertLineToList(lines[1]);
-                int inOrder = CompareTwoLists(line1, line2);
 
-                Console.WriteLine("{0} {1}", i+1, inOrder*-1);
-                if (inOrder == -1)
+                if (CompareTwoLists(line1, line2) == -1)
                 {
-                    sum += i + 2;
+                    sum += i + 1;
                 }
             }
 
@@ -55,88 +42,40 @@ namespace AdventOfCode.Solutions.Year2022
         {
             input = input.Replace("\r\n", "\n");
             string[] pairs = input.Split("\n\n");
-            long sum = 0;
-            List<List<dynamic>> sortedList = new List<List<dynamic>>();
+
+            List<List<dynamic>> everything = new List<List<dynamic>>();
             string token1 = "[[2]]";
             string token2 = "[[6]]";
             List<dynamic> token1List = ConvertLineToList(token1);
             List<dynamic> token2List = ConvertLineToList(token2);
-
-            sortedList.Add(token1List);
-            sortedList.Add(token2List);
-
-            // Fix for strange bug with input
-            string line0a = "[[[],2,9,4],[],[[[],[0]],[[7,0,10,8],[10,5,5,4,1],0,6]],[]]";
-            string line0b = "[[[8],[3,6,9,[10,4,2],[4,5]],[10,[3],[7,10,7,2],[0,5]],2]]";
-            List<dynamic> line0aList = ConvertLineToList(line0a);
-            List<dynamic> line0bList = ConvertLineToList(line0b);
-
-            int ind = sortedList.Count();
-            while (ind != 0 && CompareTwoLists(line0aList, sortedList[ind - 1]) == -1)
-            {
-                ind--;
-            }
-            sortedList.Insert(ind, line0aList);
-
-            ind = sortedList.Count();
-            while (ind != 0 && CompareTwoLists(line0bList, sortedList[ind - 1]) == -1)
-            {
-                ind--;
-            }
-            sortedList.Insert(ind, line0bList);
+            everything.Add(token1List);
+            everything.Add(token2List);
 
             for (int i = 0; i < pairs.Count(); i++)
             {
                 string[] lines = pairs[i].SplitByNewline();
                 List<dynamic> line1 = ConvertLineToList(lines[0]);
-
-                ind = sortedList.Count();
-                while (ind != 0 && CompareTwoLists(line1, sortedList[ind - 1]) == -1)
-                {
-                    ind--;
-                }
-                sortedList.Insert(ind, line1);
-
                 List<dynamic> line2 = ConvertLineToList(lines[1]);
-
-                ind = sortedList.Count();
-                while (CompareTwoLists(line2, sortedList[ind - 1]) == -1)
-                {
-                    ind--;
-                }
-                sortedList.Insert(ind, line2);
-
+                everything.Add(line1);
+                everything.Add(line2);
             }
 
-            int index1 = 0;
-            int index2 = 0;
-            for (int i = 0; i < sortedList.Count(); i++)
+            everything.Sort((x, y) => CompareTwoLists(x, y));
+            int token1Index = 0;
+            int token2Index = 0;
+            for (int i = 0; i < everything.Count(); i++)
             {
-                if (sortedList[i] == token1List)
+                if (everything[i] == token1List)
                 {
-                    index1 = i + 1;
-                }                
-                if (sortedList[i] == token2List)
+                    token1Index = i + 1;
+                }
+                if (everything[i] == token2List)
                 {
-                    index2 = i + 1;
+                    token2Index = i + 1;
                 }
             }
 
-            return (index1*index2).ToString();
-        }
-
-        public string PrintFirstOfList(List<dynamic> line)
-        {
-            dynamic current = line;
-            while (current is List<dynamic>)
-            {
-                if (current.Count == 0)
-                {
-                    return "";
-                }
-                current = current[0];
-            }
-            return current.ToString();
+            return (token1Index * token2Index).ToString();
         }
 
         public int CompareTwoLists(List<dynamic> line1, List<dynamic> line2)
@@ -166,61 +105,20 @@ namespace AdventOfCode.Solutions.Year2022
                 }
                 else if (line1[i] is long && line2[i] is List<dynamic>)
                 {
-
-
-                    dynamic current = line2[i];
-                    dynamic prev = current;
-                    bool listEmpty = false;
-                    while (current is List<dynamic>)
-                    {
-                        if (current.Count == 0)
-                        {
-                            listEmpty = true;
-                            break;
-                        }
-                        prev = current;
-                        current = current[0];
-                    }
-
-                    if (listEmpty)
-                    {
-                        return 1;
-                    }
-                    int comparison = CompareTwoLists(new List<dynamic>() { line1[i] }, prev);
+                    int comparison = CompareTwoLists(new List<dynamic>() { line1[i] }, line2[i]);
                     if (comparison != 0)
                     {
                         return comparison;
                     }
-
                 }
 
                 else if (line1[i] is List<dynamic> && line2[i] is long)
                 {
-                    dynamic current = line1[i];
-                    dynamic prev = current;
-                    bool listEmpty = false;
-                    while (current is List<dynamic>)
-                    {
-                        if (current.Count == 0)
-                        {
-                            listEmpty = true;
-                            break;
-                        }
-                        prev = current;
-                        current = current[0];
-                    }
-                    if (listEmpty)
-                    {
-
-                        return -1;
-
-                    }
-                    int comparison = CompareTwoLists(prev, new List<dynamic>() { line2[i] });
+                    int comparison = CompareTwoLists(line1[i], new List<dynamic>() { line2[i] });
                     if (comparison != 0)
                     {
                         return comparison;
                     }
-
                 }
             }
 
@@ -273,7 +171,6 @@ namespace AdventOfCode.Solutions.Year2022
                             {
                                 compList.Add(ConvertLineToList(comp));
                             }
-                            
                             comp = "";
                         } else
                         {
@@ -302,6 +199,5 @@ namespace AdventOfCode.Solutions.Year2022
 
             return compList;
         }
-
     }
 }
