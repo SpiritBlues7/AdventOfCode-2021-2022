32219e68124596415387484a3db62f925725d8be
Brian Stewart <brianstewarteditor@gmail.com>
2022-12-19 00:41:52 +1000

    2022 day 17 and 18

--- AdventOfCode/Solutions/Year2022/Day17/Day17.cs
+++ AdventOfCode/Solutions/Year2022/Day17/Day17.cs
@@ -19,14 +19,375 @@ namespace AdventOfCode.Solutions.Year2022
         protected override string SolvePartOne(string input)
         {
             input = input.Replace("\r\n", "\n");
-            string[] lines = input.SplitByNewline();
 
-            return null;
+            Dictionary<Point, char> grid = new Dictionary<Point, char>();
+            int maxHeight = 0;
+            Dictionary<int, List<Point>> rockTypes = new Dictionary<int, List<Point>>();
+            rockTypes.Add(0, new List<Point>() { new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(3, 0) });    
+            rockTypes.Add(1, new List<Point>() { new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(1, 1), new Point(1, -1) });
+            rockTypes.Add(2, new List<Point>() { new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(2, 1), new Point(2, 2) });
+            rockTypes.Add(3, new List<Point>() { new Point(0, 0), new Point(0, 1), new Point(0, 2), new Point(0, 3)});
+            rockTypes.Add(4, new List<Point>() { new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(0, 1) });
+
+            Dictionary<int, int> rockWidths = new Dictionary<int, int>()
+            {
+                { 0, 3 },
+                { 1, 2 },
+                { 2, 2 },
+                { 3, 0 },
+                { 4, 1 },
+            };
+            Dictionary<int, int> rockHeights = new Dictionary<int, int>()
+            {
+                { 0, 0 },
+                { 1, 1 },
+                { 2, 0 },
+                { 3, 0 },
+                { 4, 0 },
+            };
+
+            for (int y = 0; y < 500000; y++)
+            {
+                for (int x = 0; x < 7; x++)
+                {
+                    grid[new Point(x,y)] = '.';
+                }
+            }
+
+            int rockNum = 0;
+            int count = 0;
+            int currentHeight = maxHeight + 3;
+            int currentX = 2;
+
+            while (true)
+            {
+                if (count == 2022)
+                {
+                    break;
+                }
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (count == 2022)
+                    {
+                        break;
+                    }
+
+                    bool collidedOne = false;
+
+                    char dir = input[i];
+                    if (dir == '<')
+                    {
+
+                        if (currentX > 0 )
+                        {
+                            foreach (Point part in rockTypes[rockNum])
+                            {
+                                if (grid[new Point(currentX + part.X - 1, currentHeight + part.Y)] == '#')
+                                {
+                                    collidedOne = true;
+                                }
+                            }
+                            if (!collidedOne)
+                            {
+                                currentX--;
+                            }
+                            
+                        }
+                    }
+                    if (dir == '>')
+                    {
+
+                        if (currentX + rockWidths[rockNum] < 6)
+                        {
+                            foreach (Point part in rockTypes[rockNum])
+                            {
+                                if (grid[new Point(currentX + part.X + 1, currentHeight + part.Y)] == '#')
+                                {
+                                    collidedOne = true;
+                                }
+                            }
+                            if (!collidedOne)
+                            {
+                                currentX++;
+                            }
+                        }
+                    }
+
+                    bool collidedTwo = false;
+                    foreach (Point part in rockTypes[rockNum])
+                    {
+                        if (currentHeight + part.Y - 1 == -1 || grid[new Point(currentX + part.X, currentHeight + part.Y - 1)] == '#')
+                        {
+                            collidedTwo = true;
+                        }
+                    }
+                    if (collidedTwo)
+                    {
+                        foreach (Point part in rockTypes[rockNum])
+                        {
+                            grid[new Point(currentX + part.X, currentHeight + part.Y)] = '#';
+                            if (currentHeight + part.Y + 1> maxHeight)
+                            {
+                                maxHeight = currentHeight + part.Y + 1;
+                                Console.WriteLine(maxHeight);
+
+                            }
+                        }
+                        //for (int y = 10; y >= 0; y--)
+                        //{
+                        //    for (int x = 0; x < 7; x++)
+                        //    {
+                        //        Console.Write(grid[new Point(x, y)]);
+                        //    }
+                        //    Console.WriteLine();
+                        //}
+                        //Console.WriteLine();
+                        count++;
+                        rockNum = (rockNum + 1) % 5;
+                        currentHeight = maxHeight + 3 + rockHeights[rockNum];
+                        currentX = 2;
+                    }
+                    else
+                    {
+                        currentHeight--;
+                    }
+                }
+            }
+
+
+
+
+            return maxHeight.ToString();
         }
 
         protected override string SolvePartTwo(string input)
         {
-            return null;
+            input = input.Replace("\r\n", "\n");
+
+            Dictionary<Point, char> grid = new Dictionary<Point, char>();
+            int maxHeight = 0;
+            Dictionary<int, List<Point>> rockTypes = new Dictionary<int, List<Point>>();
+            rockTypes.Add(0, new List<Point>() { new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(3, 0) });
+            rockTypes.Add(1, new List<Point>() { new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(1, 1), new Point(1, -1) });
+            rockTypes.Add(2, new List<Point>() { new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(2, 1), new Point(2, 2) });
+            rockTypes.Add(3, new List<Point>() { new Point(0, 0), new Point(0, 1), new Point(0, 2), new Point(0, 3) });
+            rockTypes.Add(4, new List<Point>() { new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(0, 1) });
+
+            Dictionary<int, int> rockWidths = new Dictionary<int, int>()
+            {
+                { 0, 3 },
+                { 1, 2 },
+                { 2, 2 },
+                { 3, 0 },
+                { 4, 1 },
+            };
+            Dictionary<int, int> rockHeights = new Dictionary<int, int>()
+            {
+                { 0, 0 },
+                { 1, 1 },
+                { 2, 0 },
+                { 3, 0 },
+                { 4, 0 },
+            };
+
+            for (int y = 0; y < 5000000; y++)
+            {
+                for (int x = 0; x < 7; x++)
+                {
+                    grid[new Point(x, y)] = '.';
+                }
+            }
+
+            int rockNum = 0;
+            int count = 0;
+            int currentHeight = maxHeight + 3;
+            int currentX = 2;
+            Console.WriteLine((long)(24476 - 3064)*(long)70721357 + 3064 + 18204 - 3064);
+            Console.WriteLine((long)(1110053 - 40665)*(long)1447513 + 184349);
+            Console.WriteLine((long)(5402 - 2755)*(long)584795320 + 4353);
+            //1491263477320
+            //1491267631983
+            string floorXState = ".......";
+            string prevXState = ".......";
+            string prevPrevXState = ".......";
+            string prevPrevPrevXState = ".......";
+            string prevPrevPrevPrevXState = ".......";
+            List<int> vals = new List<int>();
+            Dictionary<(string, string, string, string, string, int, int), int> floorHeightsStates = new Dictionary<(string, string, string, string, string, int, int), int>();
+
+            while (true)
+            {
+
+                if (count == 1000000000)
+                {
+                    break;
+                }
+                for (int i = 0; i < input.Length; i++)
+                {
+                    //if (count % 1000 == 0)
+                    //{
+                    //    Console.WriteLine(count);
+                    //}
+                    if (count % 2800 == 0 && count != 0)
+                    {
+                        Console.WriteLine("{0}, {1}", count, currentHeight);
+                    }
+
+
+                    bool collidedOne = false;
+
+                    char dir = input[i];
+                    if (dir == '<')
+                    {
+
+                        if (currentX > 0)
+                        {
+                            foreach (Point part in rockTypes[rockNum])
+                            {
+                                if (grid[new Point(currentX + part.X - 1, currentHeight + part.Y)] == '#')
+                                {
+                                    collidedOne = true;
+                                }
+                            }
+                            if (!collidedOne)
+                            {
+                                currentX--;
+                            }
+
+                        }
+                    }
+                    if (dir == '>')
+                    {
+
+                        if (currentX + rockWidths[rockNum] < 6)
+                        {
+                            foreach (Point part in rockTypes[rockNum])
+                            {
+                                if (grid[new Point(currentX + part.X + 1, currentHeight + part.Y)] == '#')
+                                {
+                                    collidedOne = true;
+                                }
+                            }
+                            if (!collidedOne)
+                            {
+                                currentX++;
+                            }
+                        }
+                    }
+
+                    bool collidedTwo = false;
+                    foreach (Point part in rockTypes[rockNum])
+                    {
+                        if (currentHeight + part.Y - 1 == -1 || grid[new Point(currentX + part.X, currentHeight + part.Y - 1)] == '#')
+                        {
+                            collidedTwo = true;
+                        }
+                    }
+                    if (collidedTwo)
+                    {
+                        foreach (Point part in rockTypes[rockNum])
+                        {
+                            grid[new Point(currentX + part.X, currentHeight + part.Y)] = '#';
+                            if (currentHeight + part.Y + 1 > maxHeight)
+                            {
+                                maxHeight = currentHeight + part.Y + 1;
+                                //Console.WriteLine(maxHeight);
+
+                            }
+                        }
+                        //for (int y = 10; y >= 0; y--)
+                        //{
+                        //    for (int x = 0; x < 7; x++)
+                        //    {
+                        //        Console.Write(grid[new Point(x, y)]);
+                        //    }
+                        //    Console.WriteLine();
+                        //}
+                        //Console.WriteLine();
+                        count++;
+                        rockNum = (rockNum + 1) % 5;
+                        currentHeight = maxHeight + 3 + rockHeights[rockNum];
+                        currentX = 2;
+                        //Console.WriteLine("{0}, {1}, {2}", count, count / 2020, maxHeight);
+                        //for (int y = currentHeight; y >= 0; y--)
+                        //{
+                        //    for (int x = 0; x < 7; x++)
+                        //    {
+                        //        Console.Write(grid[new Point(x, y)]);
+                        //    }
+                        //    Console.WriteLine();
+                        //}
+                        //Console.WriteLine();
+                        if (currentHeight > 10)
+                        {
+                            floorXState = "";
+                            prevXState = "";
+                            prevPrevXState = "";
+                            prevPrevPrevXState = "";
+                            prevPrevPrevPrevXState = "";
+                            for (int x = 0; x < 7; x++)
+                            {
+                                floorXState += grid[new Point(x, maxHeight - 1)];
+                                prevXState += grid[new Point(x, maxHeight - 2)];
+                                prevPrevXState += grid[new Point(x, maxHeight - 3)];
+                                prevPrevPrevXState += grid[new Point(x, maxHeight - 4)];
+                                prevPrevPrevPrevXState += grid[new Point(x, maxHeight - 5)];
+                            }
+                            if (floorHeightsStates.ContainsKey((floorXState, prevXState, prevPrevXState, prevPrevPrevXState, prevPrevPrevPrevXState, rockNum, i + 1)))
+                            {
+                                Console.WriteLine("{0}, {1}, {2}, {3}", count, maxHeight, rockNum, i + 1);
+                                floorHeightsStates = new Dictionary<(string, string, string, string, string, int, int), int>();
+
+                            }
+                            floorHeightsStates[(floorXState, prevXState, prevPrevXState, prevPrevPrevXState, prevPrevPrevPrevXState, rockNum, i + 1)] = maxHeight;
+                        }
+
+                        if (count % 2020 == 0 && count != 0 && count >= 2020)
+                        {
+                            if (grid[new Point(3, maxHeight - 1)] == '#' &&
+                                grid[new Point(3, maxHeight - 2)] == '#' &&
+                                grid[new Point(3, maxHeight - 3)] == '#' &&
+                                grid[new Point(3, maxHeight - 4)] == '#' &&
+                                grid[new Point(2, maxHeight - 4)] == '#' &&
+                                grid[new Point(1, maxHeight - 4)] == '#' &&
+                                grid[new Point(4, maxHeight - 4)] == '#' &&
+                                grid[new Point(2, maxHeight - 2)] == '#' &&
+                                grid[new Point(4, maxHeight - 2)] == '#'
+                                )
+                            {
+
+                            }
+                            if (((count) / 2020 - 13) % 342 == 0)
+                            {
+                                if (vals.Contains(((count) / 2020 - 13) / 2))
+                                {
+                                    //Console.WriteLine("###################################");
+                                }
+                                vals.Add((count) / 2020 - 13);
+                                //Console.WriteLine("{0}, {1}, {2}", count, (count) / 2020 - 13, maxHeight);
+                            }
+
+
+
+                            //for (int y = currentHeight; y >= currentHeight - 20; y--)
+                            //{
+                            //    for (int x = 0; x < 7; x++)
+                            //    {
+                            //        Console.Write(grid[new Point(x, y)]);
+                            //    }
+                            //    Console.WriteLine();
+                            //}
+                            //Console.WriteLine();
+                        }
+
+                    }
+                    else
+                    {
+                        currentHeight--;
+                    }
+                }
+            }
+            return maxHeight.ToString();
         }
     }
 }
--- AdventOfCode/Solutions/Year2022/Day18/Day18.cs
+++ AdventOfCode/Solutions/Year2022/Day18/Day18.cs
@@ -22,12 +22,179 @@ namespace AdventOfCode.Solutions.Year2022
             input = input.Replace("\r\n", "\n");
             string[] lines = input.SplitByNewline();
 
-            return null;
+            long total = lines.Count() * 6;
+            List<(int, int, int)> cubes = new List<(int, int, int)>();
+            foreach (string line in lines)
+            {
+                string[] comp = line.Split(",");
+                int x = int.Parse(comp[0]);
+                int y = int.Parse(comp[1]);
+                int z = int.Parse(comp[2]);
+                cubes.Add((x, y, z));
+            }
+
+            foreach ((int, int, int) cube1 in cubes)
+            {
+                foreach ((int, int, int) cube2 in cubes)
+                {
+                    if (cube1.Item1 == cube2.Item1 && cube1.Item2 == cube2.Item2 && cube1.Item3 != cube2.Item3 && Math.Abs(cube1.Item3 - cube2.Item3) <= 1)
+                    {
+                        total--;
+                    }
+                    if (cube1.Item2 == cube2.Item2 && cube1.Item3 == cube2.Item3 && cube1.Item1 != cube2.Item1 && Math.Abs(cube1.Item1 - cube2.Item1) <= 1)
+                    {
+                        total--;
+                    }
+                    if (cube1.Item1 == cube2.Item1 && cube1.Item3 == cube2.Item3 && cube1.Item2 != cube2.Item2 && Math.Abs(cube1.Item2 - cube2.Item2) <= 1)
+                    {
+                        total--;
+                    }
+                }
+            }
+
+            return total.ToString();
         }
 
         protected override string SolvePartTwo(string input)
         {
-            return null;
+            input = input.Replace("\r\n", "\n");
+            string[] lines = input.SplitByNewline();
+
+            long total = lines.Count() * 6;
+            HashSet<(int, int, int)> cubes = new HashSet<(int, int, int)>();
+            foreach (string line in lines)
+            {
+                string[] comp = line.Split(",");
+                int x = int.Parse(comp[0]);
+                int y = int.Parse(comp[1]);
+                int z = int.Parse(comp[2]);
+                cubes.Add((x, y, z));
+            }
+
+            foreach ((int, int, int) cube1 in cubes)
+            {
+                foreach ((int, int, int) cube2 in cubes)
+                {
+                    if (cube1.Item1 == cube2.Item1 && cube1.Item2 == cube2.Item2 && cube1.Item3 != cube2.Item3 && Math.Abs(cube1.Item3 - cube2.Item3) <= 1)
+                    {
+                        total--;
+                    }
+                    if (cube1.Item2 == cube2.Item2 && cube1.Item3 == cube2.Item3 && cube1.Item1 != cube2.Item1 && Math.Abs(cube1.Item1 - cube2.Item1) <= 1)
+                    {
+                        total--;
+                    }
+                    if (cube1.Item1 == cube2.Item1 && cube1.Item3 == cube2.Item3 && cube1.Item2 != cube2.Item2 && Math.Abs(cube1.Item2 - cube2.Item2) <= 1)
+                    {
+                        total--;
+                    }
+                }
+            }
+
+            HashSet<(int, int, int)> airPockets = new HashSet<(int, int, int)>();
+            for (int x = 0; x < 20; x++)
+            {
+                for (int y = 0; y < 20; y++)
+                {
+                    for (int z = 0; z < 20; z++)
+                    {
+                        if (x == 15 && y == 15 && z == 15)
+                        {
+                            Console.WriteLine();
+                        }
+                        int edgesTouching = 0;
+                        bool isCube = false;
+                        foreach ((int, int, int) cube in cubes)
+                        {
+                            if (cube.Item1 == x && cube.Item2 == y && cube.Item3 == z)
+                            {
+                                isCube = true;
+                                break;
+                            }
+                        }
+
+                        if (isCube)
+                        {
+                            continue;
+                        }
+                        (bool, int) resultXp = checkDirectionForCube(cubes, x, y, z, 1, 0, 0);
+                        (bool, int) resultXm = checkDirectionForCube(cubes, x, y, z, -1, 0, 0);
+                        (bool, int) resultYp = checkDirectionForCube(cubes, x, y, z, 0, 1, 0);
+                        (bool, int) resultYm = checkDirectionForCube(cubes, x, y, z, 0, -1, 0);
+                        (bool, int) resultZp = checkDirectionForCube(cubes, x, y, z, 0, 0, 1);
+                        (bool, int) resultZm = checkDirectionForCube(cubes, x, y, z, 0, 0, -1);
+                        if (resultXp.Item1) { edgesTouching++; }
+                        if (resultXm.Item1) { edgesTouching++; }
+                        if (resultYp.Item1) { edgesTouching++; }
+                        if (resultYm.Item1) { edgesTouching++; }
+                        if (resultZp.Item1) { edgesTouching++; }
+                        if (resultZm.Item1) { edgesTouching++; }
+
+
+                        if (edgesTouching == 6)
+                        {
+                            airPockets.Add((x, y, z));
+                            Console.WriteLine("{0} {1} {2}", x, y, z);
+                            total -= 6;
+                            //if (resultXp.Item2 == 0) { total--; }
+                            //if (resultXm.Item2 == 0) { total--; }
+                            //if (resultYp.Item2 == 0) { total--; }
+                            //if (resultYm.Item2 == 0) { total--; }
+                            //if (resultZp.Item2 == 0) { total--; }
+                            //if (resultZm.Item2 == 0) { total--; }
+                        }
+                    }
+                }
+            }
+
+            foreach ((int, int, int) cube1 in airPockets)
+            {
+                foreach ((int, int, int) cube2 in airPockets)
+                {
+                    if (cube1.Item1 == cube2.Item1 && cube1.Item2 == cube2.Item2 && cube1.Item3 != cube2.Item3 && Math.Abs(cube1.Item3 - cube2.Item3) <= 1)
+                    {
+                        total++;
+                    }
+                    if (cube1.Item2 == cube2.Item2 && cube1.Item3 == cube2.Item3 && cube1.Item1 != cube2.Item1 && Math.Abs(cube1.Item1 - cube2.Item1) <= 1)
+                    {
+                        total++;
+                    }
+                    if (cube1.Item1 == cube2.Item1 && cube1.Item3 == cube2.Item3 && cube1.Item2 != cube2.Item2 && Math.Abs(cube1.Item2 - cube2.Item2) <= 1)
+                    {
+                        total++;
+                    }
+                }
+            }
+
+            return total.ToString();
+        }
+
+        private static (bool, int) checkDirectionForCube(HashSet<(int, int, int)> cubes, int x, int y, int z, int Xmodifier, int Ymodifier, int Zmodifier)
+        {
+            int airCount = 0;
+            while (x >= 0 && y >= 0 && z >= 0 && x <= 20 && y <= 20 && z <= 20)
+            {
+                bool foundCube = false;
+                foreach ((int, int, int) cube in cubes)
+                {
+                    airCount = 0;
+                    if (cube.Item1 == x && cube.Item2 == y && cube.Item3 == z)
+                    {
+                        foundCube = true;
+                        break;
+                    } else
+                    {
+                        airCount++;
+                    }
+                }
+                if (foundCube)
+                {
+                    return (true, airCount);
+                }
+                x += Xmodifier;
+                y += Ymodifier;
+                z += Zmodifier;
+            }
+            return (false, 0);
         }
     }
 }
